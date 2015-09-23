93164f69cae66ab506f831f3a7ddb79986d8941b
Jérémy Desvages <jejem@phyrexia.org>
2015-09-23 13:20:04 +0200

    Adding Phyrexia\Log\LogLevel v1.0.0

---
+++ LogLevel.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Log/LogLevel.php
+ *
+ * @author Jérémy 'Jejem' Desvages <jejem@phyrexia.org>
+ * @copyright Jérémy 'Jejem' Desvages
+ * @license The MIT License (MIT)
+ * @version 1.0.0
+**/
+
+namespace Phyrexia\Log;
+
+class LogLevel {
+	const EMERGENCY = 'emergency';
+	const ALERT     = 'alert';
+	const CRITICAL  = 'critical';
+	const ERROR     = 'error';
+	const WARNING   = 'warning';
+	const NOTICE    = 'notice';
+	const INFO      = 'info';
+	const DEBUG     = 'debug';
+}
